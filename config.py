e27e01790863c857404ac8b69985df1f12f31b3e
Ivan Syrovoiskii <syrvanser@gmail.com>
2019-03-10 04:22:57 +0000

    wip

---
+++ config.py
@@ -0,0 +1,22 @@
+from utils import DotDict
+
+args = DotDict({
+    'lr': 0.001,
+    'dropout': 0.3,
+    'epochs': 10,
+    'batch_size': 64,
+    'num_channels': 512,
+    'filter_size': 3,
+    'mcts_iterations': 50,  # num of mcts sims
+    'max_depth': 200,
+    'max_example_games': 10,
+    'num_epochs': 100,
+    'c_puct': 1,
+    'max_examples_len': 100000,  # train examples
+    'threshold': 0.6,
+    'max_example_history_len': 20,  # global examples
+    'example_iter_number': 0,
+    'move_count_limit': 500,
+    'compare_rounds': 50,
+    'processes': 4
+})
